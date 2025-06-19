1207128706e69a97f8e47a6df51f7d352c9b561a
Shubham <shubhamrajchauhan07@gmail.com>
2025-06-19 20:46:11 +0530

    custom login

--- SpringSecEx/src/main/java/com/shubham/SpringSecEx/HelloController.java
+++ SpringSecEx/src/main/java/com/shubham/SpringSecEx/HelloController.java
@@ -1,6 +1,7 @@
 package com.shubham.SpringSecEx;
 
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String greet() {
-        return "Welcome to my World";
+    public String greet(HttpServletRequest request) {
+        return "Welcome to my World" + request.getSession().getId();
     }
 }
 
