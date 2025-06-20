61a90ec2fce054140e443549c1b9ec7b35aa7406
Shubham <shubhamrajchauhan07@gmail.com>
2025-06-21 01:31:06 +0530

    JWT

---
+++ SpringSecEx/src/main/java/com/shubham/SpringSecEx/service/UserService.java
@@ -0,0 +1,42 @@
+package com.shubham.SpringSecEx.service;
+
+import com.shubham.SpringSecEx.model.Users;
+import com.shubham.SpringSecEx.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    AuthenticationManager authManager;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+            return jwtService.generateToken();
+
+        return "fail";
+    }
+
+
+}
