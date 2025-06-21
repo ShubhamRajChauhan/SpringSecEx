4d313c9a35eb65e352c1b14bdf2a53c5786b29db
Shubham <shubhamrajchauhan07@gmail.com>
2025-06-21 16:04:19 +0530

    Validating JWT Token

--- SpringSecEx/src/main/java/com/shubham/SpringSecEx/config/SecurityConfig.java
+++ SpringSecEx/src/main/java/com/shubham/SpringSecEx/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,10 @@ public class SecurityConfig {
 
     @Autowired
     public UserDetailsService userDetailsService;
+
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
 
     @Bean
@@ -47,6 +52,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
 
